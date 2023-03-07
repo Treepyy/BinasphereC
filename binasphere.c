4f80568cbfb02325ca643d6b3a0b0bec2a86486a
Treepyy <vancegyan29@gmail.com>
2023-03-07 08:54:17 +0800

    added formula conversion: for when total words exceed given formula

--- binasphere.c
+++ binasphere.c
@@ -28,6 +28,24 @@ void splitSentence(string sentence, stringArr words, int* totalWords){
     *totalWords = j;
 }
 
+int wordCount(string sentence){
+    int j = 0, k = 0;
+    while(k <= strlen(sentence)){
+        
+        if (sentence[k] == ' '){
+            j++;
+        }
+        
+        if (sentence[k] == '\0'){
+            j++;
+        }
+        
+        k++;
+    }
+    
+    return j;
+}
+
 int scanFormula(string formula){
 	int i;
 	for (i = 0; i < strlen(formula); i++){
@@ -62,11 +80,28 @@ void binasphereSplit(string chorus, string formula, stringArr chorusLines[2], in
 	
 	
 }
+
+void formulaRepeater(string formula, string newFormula, int totalWords){
+  int size = totalWords;
+  char repeated[size];
+  int i = 0, j = 0;
+  while (i < size){
+    repeated[i] = formula[j];
+    i++;
+    j++;
+    if (j >= strlen(formula)){
+    	j = 0;	
+	}
+  }
+  
+  strcpy(newFormula, repeated);
+}
+
 int main()
 {
-    string sentenceInput, formulaInput;
+    string sentenceInput, formulaInput, newFormula;
     stringArr chorusLines[2];
-    int i, totalLine0, totalLine1;
+    int i, totalLine0, totalLine1, totalWords;
     
     printf("Enter Binasphere Chorus:\n=>");
     scanf(" %[^\n]", sentenceInput);
@@ -83,13 +118,27 @@ int main()
     		invalidFormula = scanFormula(formulaInput);
 		}
 	}
-	binasphereSplit(sentenceInput, formulaInput, chorusLines, &totalLine0, &totalLine1);
+	
+	totalWords = wordCount(sentenceInput);
+	
+	if (totalWords > strlen(formulaInput)){
+		formulaRepeater(formulaInput, newFormula, totalWords);
+		binasphereSplit(sentenceInput, newFormula, chorusLines, &totalLine0, &totalLine1);
+		
+		printf("\nCONVERTED FORMULA:\nEXEC hymme 2X1/0>>");
+		printf("%s\n", newFormula);
+	}
+	
+	else {
+		binasphereSplit(sentenceInput, formulaInput, chorusLines, &totalLine0, &totalLine1);
+	}
+	
 	printf("\nDEFRAGMENTED CHORUS:\n");
 	for (i = 0; i < totalLine0; i++){
 	    printf("%s ", chorusLines[0][i]);
 	}
 	printf("\n");
-	for (i = 0; i < totalLine0; i++){
+	for (i = 0; i < totalLine1; i++){
 	    printf("%s ", chorusLines[1][i]);
 	}
 
